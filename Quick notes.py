67c6bf6a4730ee3f82773d5846a35bf3e4a2ce76
moizcfd <f219505@cfd.nu.edu.pk>
2024-01-04 23:10:37 +0500

    Updated Notes for Numpy Library

--- Quick notes.py
+++ Quick notes.py
@@ -1,79 +1,41 @@
-# Guideline:https://www.youtube.com/watch?v=QUT1VHiLmmI
-
-# numpy self learned
-# numpy faster as it uses fixed types
+# Quick notes
 import numpy as np
-a = np.array([1,2,3])
-# print(a)
 
-# can specify which type of data to store:
-a = np.array([1,2,3], dtype='int32')
-b = np.array([[9.0,8.7,4.6],[2.4,5.4,9.8]])
-# print(b)
-# getting dimensions for array
-a.ndim
-# getting shape of arr
-b.shape
-# get type
-a.dtype
-# getsize
-a.itemsize
-# get totalsize (a.itemsize * a.size) 
-a.nbytes
-b.nbytes
+## Create arr:
+arr = np.array([1,2,3])
+arr = np.full((2,2),1,dtype='int')
 
+## Check arr info:
+# For array dimension
+arr.ndim 
+# For array Shape:
+arr.shape
+# For reshaping:
+arr.reshape(size)
 
+## Accessing in arr:
 
+# get specific row,col value 
+arr[1,5]
 
-# Accessing specific elements
-newarr= np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
-newarr.ndim
-newarr.shape
-newarr.dtype
-# getting specific element [r,c]
-newarr[1,5]
-# Same can be done by -ve index
-# i.e. newarr[1,-2] (second row second last col)
+# get specific row:
+arr[0,:]
+# get specific col:
+arr[:,0]
 
-# get a specific row (row num, : for slicing whole row):
-newarr[0,:] 
+# get specific row(s) and col(s) with step size:
+arr[0:2:1,0:2:1]
 
-# get a specific col (for slicing whole col : , row num ):
-newarr[:,2] 
+## Initializing arr:
 
-# get a specific col (for slicing whole col : , row num ):
-newarr[:,2]
-
-# specific rows and col [startindex:endindex:stepsize]
-newarr[0, 1:6:1]
-
-# Access rows 0 to 1 and columns 4 to 6
-subset = newarr[0:2, 3:6]
-print(subset)
-
-# changing vals:
-newarr[1,5] = 20
-newarr[:,2] = 5
-
-
-
-
-# checking 3d functions
-b = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
-print(b)
-
-b[0,1,1]
-
- 
- # initializing different arrs
 # any number
 arr3 = np.full((2, 2), 1, dtype="int32")
-# print(arr3)
-# full_like(to use shape of a previously built arr)
 
+# full_like(to use shape of a previously built arr)
+# (arr_to_shape_like,number,datatype)
 np.full_like(arr3,4,dtype="int32")
 
-# rand numbers
+# rand numbers (row,col)
 np.random.rand(4,2)
 
 # rand numbers according to previously built shape:
@@ -82,8 +44,7 @@ np.random.random_sample(arr3.shape)
 # random int generation(start,end,nxn arr)
 np.random.randint(1,7,size=(3,3))
 
-
-
+## Maths:
 # identity matrix
 np.identity(5)
 # repeat an array
@@ -91,22 +52,12 @@ arr = np.array([1,2,3])
 r1=np.repeat(arr,3,axis=0)
 print(r1)
 
-
-
-# quiz
-arr = np.full((5,5),1)
-arr[1:4,1:4]=0
-arr[2,2]=9
-print(arr)
-
 # to avoid changing value of a when changing b, use copy
 a=np.array([1,2,3])
 b=a.copy()
 b[1]=0
 print(a)
 print(b)
-
-
 
 # Basic maths in numpy:
 a = np.array([1,2,3,4])
@@ -125,26 +76,25 @@ np.sin(a)
 np.cos(a)
 np.tan(a)
 
-
-
 # LA
 a= np.full((2,3),1)
 b=np.full((3,2),2)
 print(a)
 print(b)
 
+# somewhat similar to np.dot
 np.matmul(a,b)
-# np.dot
-
-
-
+# Create identity matrix
 c = np.identity(3)
 # determinant
 np.linalg.det(c)
 
 
+## Operations to be performed on arrays:
+arr+arr
+# All others like -,/,* can be performed similarly
 
-#Stats
+##Stats
 arr=np.array([[1,2,3],[4,5,6]])
 # for overall
 np.min(arr)
@@ -156,7 +106,10 @@ np.max(arr)
 np.sum(arr)
 
 
+# Dot product:
+np.dot(arr,arr2)
 
+## Reorganizing:
 
 # reorganizing/reshaping arr:
 before = np.array([[1,2,3],[4,5,6]])
@@ -170,9 +123,7 @@ np.vstack([before,after,before])
 
 np.hstack([before,after])
 
-
-
-# Loading data from file:
+## Loading data from file:
 filedata = np.genfromtxt('C:\\Users\\MOIZ UDDIN\\Desktop\\SampleFileLoad.txt', delimiter=",")
 
 # Remove NaN values
@@ -180,8 +131,6 @@ cleaned_data = filedata[~np.isnan(filedata)]
 
 cleaned_data.astype('int32')
 print(cleaned_data)
-
-
 
 # Advanced Indexing:
 # Shows where in arr values are > 50
@@ -198,3 +147,12 @@ np.any(cleaned_data > 50, axis = 0)
 
 np.all(cleaned_data > 50, axis = 0)
 # ~ means not
+
+## Extra func:
+# get type
+a.dtype
+# getsize
+a.itemsize
+# get totalsize (a.itemsize * a.size) 
+a.nbytes
+b.nbytes
